f87d93cba587fb53e09d06e82b6220869c6663ad
sangeetmore <san.mor.rt20@dypatil.edu>
2023-04-27 22:54:46 +0530

    :zap: added connUrl as a CONSTANT

--- src/components/ChatPlugin.vue
+++ src/components/ChatPlugin.vue
@@ -1,13 +1,13 @@
 <template>
-    <div id="rasa-chat-widget" data-websocket-url="https://f534-43-242-224-42.in.ngrok.io" data-avatar-url="https://icon2.cleanpng.com/20171220/xxq/smiley-png-5a3a6ae39e2dd7.0296809115137778916479.jpg"></div>
+    <div id="rasa-chat-widget" data-websocket-url="" data-avatar-url="https://icon2.cleanpng.com/20171220/xxq/smiley-png-5a3a6ae39e2dd7.0296809115137778916479.jpg"></div>
 </template>
 
 <script>
+import CONSTANTS from '../constants/constants';
 export default{
     name: 'ChatPlugin',
     data:()=>({
-        // connUrl: "http://localhost:5005",
-        // avatarUrl: "../assets/img/bot-logo.png",
+        connUrl: CONSTANTS.connUrl,
     }),
     mounted() {
       let recaptchaScript = document.createElement('script')
